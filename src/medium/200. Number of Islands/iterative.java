936e5c4f559ff401fbb5e86a777d271b5b12fdf5
Andrii Korzh <korzh.andrii@gmail.com>
2019-06-16 18:50:53 -0700

    [200,java] reformat

--- src/medium/200. Number of Islands/iterative.java
+++ src/medium/200. Number of Islands/iterative.java
@@ -2,17 +2,18 @@ class Solution {
 	int count = 0;
 	int rows;
 	int columns;
-	char [][]grid;
+	char[][] grid;
 	Deque<int[]> stack = new ArrayDeque();
-	public int numIslands( char[][] grid) {
-		if( grid == null || grid.length == 0) return 0;
+
+	public int numIslands(char[][] grid) {
+		if (grid == null || grid.length == 0) return 0;
 		this.grid = grid;
 		rows = grid.length;
 		columns = grid[0].length;
-		for( int r = 0; r < rows; r++){
-			for( int c = 0; c < columns; c++){
-				if( grid[r][c] == '1'){
-					stack.push(new int[]{r,c});
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < columns; c++) {
+				if (grid[r][c] == '1') {
+					stack.push(new int[]{r, c});
 					traverse();
 					count++;
 				}
@@ -21,20 +22,20 @@ class Solution {
 		return count;
 	}
 
-	void traverse(){
-		while(!stack.isEmpty()) {
-			int[]pair = stack.pop();
+	void traverse() {
+		while (!stack.isEmpty()) {
+			int[] pair = stack.pop();
 			int r = pair[0];
 			int c = pair[1];
-			if( r < 0 || r >= rows) continue;
-			if( c < 0 || c >= columns) continue;
-			if( grid[r][c] == '0') continue;
+			if (r < 0 || r >= rows) continue;
+			if (c < 0 || c >= columns) continue;
+			if (grid[r][c] == '0') continue;
 
 			grid[r][c] = '0';
-			stack.push(new int[]{r+1,c});
-			stack.push(new int[]{r-1,c});
-			stack.push(new int[]{r,c+1});
-			stack.push(new int[]{r,c-1});
+			stack.push(new int[]{r + 1, c});
+			stack.push(new int[]{r - 1, c});
+			stack.push(new int[]{r, c + 1});
+			stack.push(new int[]{r, c - 1});
 		}
 	}
 }

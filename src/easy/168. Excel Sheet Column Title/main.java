d8c868ed5843a81d5d4b0eb714058c10d5e55b11
Andrii Korzh <korzh.andrii@gmail.com>
2019-08-17 10:05:23 -0700

    [168,java]: cleanup

--- src/easy/168. Excel Sheet Column Title/main.java
+++ src/easy/168. Excel Sheet Column Title/main.java
@@ -1,10 +1,13 @@
 class Solution {
+
+	static final int ALHPABET_LEN = 26;
+
 	public String convertToTitle(int n) {
 		String r = "";
 		while (n > 0) {
 			n--;
-			r = (char) (n % 26 + 'A') + r;
-			n /= 26;
+			r = (char) (n % ALHPABET_LEN + 'A') + r;
+			n /= ALHPABET_LEN;
 		}
 		return r;
 	}
